36fa4c4c46ae8c73626f4c48ecd32f7bc9bcec88
xhetler <dusan.hetlerovic@gmail.com>
2019-12-05 16:39:51 +0100

    changes to script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,35 +1,53 @@
-cd ..
 source venv/bin/activate
 FILE_DATASET_SPLIT="datasets-split-exp.csv"
-FILE_DATASET_RM_O="datasets-rm-o-exp.csv"
+FILE_DATASET_RM="datasets-rm-exp.csv"
 NOW=""
+NOWF=""
+
 now(){
-	NOW=$(date +"%Y%m%d-%H%M%S")
+	NOW=$(date +"%d/%m/%Y-%H:%M:%S")
 }
-now
-TIMESTAMP_SPLIT="$NOW"
-pv056-split-data -c configs/split/default.json -d "$FILE_DATASET_SPLIT" > "scripts/log-$TIMESTAMP_SPLIT-split.log"
-
-echo "SPLIT completed $NOW"
-now
-TIMESTAMP_OD="$NOW"
-pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$TIMESTAMP_OD-od.log"
-echo "OD completed $NOW"
+nowf(){
+  NOWF=$(date +"%d%m%Y-%H%M%S")
+}
 
 now
-TIMESTAMP_RM_O="$NOW"
-pv056-remove-outliers -c configs/rm_o/default.json -d "$FILE_DATASET_RM_O" > "scripts/log-$TIMESTAMP_RM_O-rm_o.log"
-echo "RM O completed $NOW"
-
+nowf
+TIMESTAMP_SPLIT="$NOWF"
+printf "%s - Splitting data...\n" "$NOW"
+pv056-split-data -c configs/split/default.json -d "$FILE_DATASET_SPLIT" > "logs/log-$TIMESTAMP_SPLIT-split.log"
 now
-TIMESTAMP_CLF="$NOW"
-pv056-run-clf -c configs/clf/default.json -d "$FILE_DATASET_RM_O" > "scripts/log-$TIMESTAMP_CLF-clf.log"
-echo "CLF completed $NOW"
+printf "%s - SPLIT done.\n\n" "$NOW"
 
+nowf
+TIMESTAMP_OD="$NOWF"
+printf "%s - Applying OD methods...\n" "$NOW"
+pv056-apply-od-methods -c configs/od/default.json > "logs/log-$TIMESTAMP_OD-od.log"
 now
-TIMESTAMP_ACC="$NOW"
-pv056-statistics -r clf_outputs/ > "accurracy-$TIMESTAMP_ACC.csv"
-echo "STATS completed $NOW"
+printf "%s - OD done.\n\n" "$NOW"
+
+nowf
+TIMESTAMP_RM="$NOWF"
+printf "%s - Removing outliers...\n" "$NOW"
+pv056-remove-outliers -c configs/rm/default.json -d "$FILE_DATASET_RM" > "logs/log-$TIMESTAMP_RM-rm.log"
+now
+printf "%s - RM done.\n\n" "$NOW"
+
+nowf
+TIMESTAMP_CLF="$NOWF"
+printf "%s - Running classification...\n" "$NOW"
+pv056-run-clf -c configs/clf/default.json -d "$FILE_DATASET_RM" > "scripts/log-$TIMESTAMP_CLF-clf.log"
+now
+printf "%s - CLF done.\n\n" "$NOW"
+
+nowf
+TIMESTAMP_ACC="$NOWF"
+printf "%s - Counting accuracy...\n" "$NOW"
+pv056-statistics -c configs/stats/default.json > "accurracy-$TIMESTAMP_ACC.csv"
+now
+printf "%s - ACC done.\n\n" "$NOW"
+
+echo "Script finished."
 
 
 
