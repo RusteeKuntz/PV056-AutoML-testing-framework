1560bddf714058e96542f881aa1842a888aa0d62
Adam Bajger <xbajger@fi.muni.cz>
2019-12-15 18:17:54 +0100

    accidentally commited script sh before

--- scripts/script.sh
+++ scripts/script.sh
@@ -16,28 +16,28 @@ now
 nowf
 TIMESTAMP_SPLIT="$NOWF"
 printf "%s - Splitting data...\n" "$NOW"
-pv056-split-data -c configs/split/default.json -d "$FILE_DATASET_SPLIT" > "logs/log-split-$TIMESTAMP_SPLIT.log"
+#pv056-split-data -c configs/split/default.json -d "$FILE_DATASET_SPLIT" > "logs/log-split-$TIMESTAMP_SPLIT.log"
 now
 printf "%s - SPLIT done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_OD="$NOWF"
 printf "%s - Applying OD methods...\n" "$NOW"
-pv056-apply-od-methods -c configs/od/default.json > "logs/log-od-$TIMESTAMP_OD.log"
+#pv056-apply-od-methods -c configs/od/default.json > "logs/log-od-$TIMESTAMP_OD.log"
 now
 printf "%s - OD done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_RM="$NOWF"
 printf "%s - Removing outliers...\n" "$NOW"
-pv056-remove-outliers -c configs/rm/default.json -d "$FILE_DATASET_RM" > "logs/log-rm-$TIMESTAMP_RM.log"
+#pv056-remove-outliers -c configs/rm/default.json -d "$FILE_DATASET_RM" > "logs/log-rm-$TIMESTAMP_RM.log"
 now
 printf "%s - RM done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_CLF="$NOWF"
 printf "%s - Running classification...\n" "$NOW"
-pv056-run-clf -c configs/clf/default.json -d "$FILE_DATASET_RM" > "logs/log-clf-$TIMESTAMP_CLF.log"
+#pv056-run-clf -c configs/clf/default.json -d "$FILE_DATASET_RM" > "logs/log-clf-$TIMESTAMP_CLF.log"
 now
 printf "%s - CLF done.\n\n" "$NOW"
 
