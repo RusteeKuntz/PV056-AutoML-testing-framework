e5f2628662005a46a7bae2f2aa1d955145b31d44
AdamBajger <adambaj@seznam.cz>
2020-03-13 16:43:23 +0100

    schema type fix

--- pv056_2019/schemas.py
+++ pv056_2019/schemas.py
@@ -122,6 +122,5 @@ class FeatureSelectionConfigurationSchema(BaseModel):
 class FeatureSelectionStepSchema(BaseModel):
     weka_jar_path: str
     output_folder_path: str
-    fs_mapping_csv_path: str
     selection_methods: List[FeatureSelectionConfigurationSchema]
 
