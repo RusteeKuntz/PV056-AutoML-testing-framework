374bb85f1e288229fce26f8b922dddcd71f7ff98
AdamBajger <adambaj@seznam.cz>
2020-03-20 21:53:37 +0100

    Upgraded DataSplitter so that user ca now: - perform m*n-fold cross-validation split - control the random state - control "n" and "m" in m*n-fold split Default behavior preserves compatibility for reproducibility purposes

--- pv056_2019/data_splitter.py
+++ pv056_2019/data_splitter.py
@@ -3,6 +3,7 @@ import csv
 import json
 import os
 import sys
+import random
 
 from sklearn.model_selection import KFold
 
@@ -12,7 +13,7 @@ from pv056_2019.schemas import SplitterSchema
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Script splits datasets for cross-validation"
+        description="Script splits datasets for m*k-fold cross-validation. Script splits datasets m-times into k folds."
     )
     parser.add_argument("--config-file", "-c", required=True, help="JSON configuration")
     parser.add_argument(
@@ -29,30 +30,46 @@ def main():
 
     data_loader = DataLoader(conf.data_path)
 
+    # if number of k-fold repeats is one, behaviour is same as in previous versions
+    if conf.m_of_repeats > 1:
+        # if number of repeats is greater than 1, only then initialize random values for splitting.
+        # For compatibility purposes
+        r = random.Random(conf.random_state)
+        # this way the random states cannot repeat (the measure is probably not necessary, but safe is safe)
+        # random states have to be integers, or numpy RandomState instances
+        randomness_space = 10000
+        pseudo_random_states = [r.randint(randomness_space * m, randomness_space * (m+1)) for m in range(conf.m_of_repeats)]
+    else:
+        pseudo_random_states = [conf.random_state]
+
     datasets_output = []
     try:
         for dataframe in data_loader.load_files():
             print("Splitting:", dataframe._arff_data["relation"], flush=True)
             dataframe = dataframe.add_index_column()
-            kfold = KFold(n_splits=5, shuffle=True, random_state=42)
-            for index, data_fold in enumerate(kfold.split(dataframe.index.values)):
-                train_index, test_index = data_fold
+            for m in range(conf.m_of_repeats):
 
-                train_frame = dataframe.select_by_index(train_index)
-                train_name = (
-                    dataframe._arff_data["relation"] + "_" + str(index) + "_train.arff"
-                )
-                train_split_output = os.path.join(conf.train_split_dir, train_name)
-                train_frame.arff_dump(train_split_output)
+                # previously, in early versions, the random_state here was 42. This number was moved into SplitterSchema
+                # datasets_file a default value, when AdamBajger was upgrading the splitting method.
+                kfold = KFold(n_splits=conf.k_of_folds, shuffle=True, random_state=pseudo_random_states[m])
+                for k, data_fold in enumerate(kfold.split(dataframe.index.values)):
+                    train_index, test_index = data_fold
 
-                test_frame = dataframe.select_by_index(test_index)
-                test_name = (
-                    dataframe._arff_data["relation"] + "_" + str(index) + "_test.arff"
-                )
-                test_split_output = os.path.join(conf.test_split_dir, test_name)
-                test_frame.arff_dump(test_split_output)
+                    train_frame = dataframe.select_by_index(train_index)
+                    train_name = (
+                        dataframe._arff_data["relation"] + "_" + str(k) + "-" + str(m) + "_train.arff"
+                    )
+                    train_split_output = os.path.join(conf.train_split_dir, train_name)
+                    train_frame.arff_dump(train_split_output)
 
-                datasets_output.append([train_split_output, test_split_output, ""])
+                    test_frame = dataframe.select_by_index(test_index)
+                    test_name = (
+                        dataframe._arff_data["relation"] + "_" + str(k) + "-" + str(m) + "_test.arff"
+                    )
+                    test_split_output = os.path.join(conf.test_split_dir, test_name)
+                    test_frame.arff_dump(test_split_output)
+
+                    datasets_output.append([train_split_output, test_split_output, ""])
 
     except KeyboardInterrupt:
         print("\nInterupted!", flush=True, file=sys.stderr)
