36c6314a4f47c841b7aa982d59f766269e32c85a
AdamBajger <adambaj@seznam.cz>
2020-04-07 22:11:05 +0200

    fixing performing operations on closed files (wrong indentation)

--- pv056_2019/remove_outliers.py
+++ pv056_2019/remove_outliers.py
@@ -41,99 +41,97 @@ def main():
         processed_base = []
         with open(args["datasets_csv_in"], "r") as csv_in:
             reader = csv.reader(csv_in)
+            for row in reader:
+                train_file_path = row[0]
+                test_file_path = row[1]
+                configs_history = row[2:]
+                preceeding_config_json = configs_history[-1]
+                preceeding_hex = os.path.basename(preceeding_config_json).split(".")[0]
+                dataframe = DataLoader._load_data_file(train_file_path)
 
+                if preceeding_hex == BASELINE_NAME:
+                    percentages = [0]
+                else:
+                    # skip non-baseline files that have no OD_VALUES column
+                    if OD_VALUE_NAME not in dataframe.columns:
+                        print(
+                            "Skipping {}. File does not have an OD_VALUE.".format(
+                                train_file_path
+                            ),
+                            flush=True,
+                            file=sys.stderr,
+                        )
+                        continue
 
-        for row in reader:
-            train_file_path = row[0]
-            test_file_path = row[1]
-            configs_history = row[2:]
-            preceeding_config_json = configs_history[-1]
-            preceeding_hex = os.path.basename(preceeding_config_json).split(".")[0]
-            dataframe = DataLoader._load_data_file(train_file_path)
-
-            if preceeding_hex == BASELINE_NAME:
-                percentages = [0]
-            else:
-                # skip non-baseline files that have no OD_VALUES column
-                if OD_VALUE_NAME not in dataframe.columns:
-                    print(
-                        "Skipping {}. File does not have an OD_VALUE.".format(
-                            train_file_path
-                        ),
-                        flush=True,
-                        file=sys.stderr,
-                    )
-                    continue
-
-                # if percentage is a value (not list) turn it into a list with one value
-                percentages = (
-                    conf.percentage
-                    if isinstance(conf.percentage, list)
-                    else [conf.percentage]
-                )
-
-            # if conf.keep_original and not 0 in percentages:
-            #         percentages.append(0)
-            for percentage in percentages:
-                # check if base output
-                # ds_fold_pair = os.path.basename(train_file_path).split("_")[:2]
-                # if percentage == 0:
-                #     if not ds_fold_pair in processed_base:
-                #         processed_base.append(ds_fold_pair)
-                #     else:
-                #         continue
-                try:
-                    if percentage == 0:
-                        new_frame = dataframe
-                    else:
-                        # here the outliers are removed
-                        rev = True if percentage < 0 else False
-                        new_frame = dataframe.select_by_od_quantile(1 - (percentage / 100), rev)
-                        new_frame.pop(OD_VALUE_NAME)
-                        new_frame._arff_data["attributes"] = [
-                            x
-                            for x in new_frame._arff_data["attributes"]
-                            if x[0] != OD_VALUE_NAME
-                        ]
-                    print("   ", train_file_path, "{}%".format(percentage))
-                    file_basename = os.path.basename(train_file_path)
-                    #name_split.insert(-1, "removed-{0:.2f}".format(round(percentage, 2)))
-
-                    rm_setting = {"RM": percentage}
-                    # unlike in other steps, here the setting is unique by the percent number, so no hex is needed
-                    identifier = "RM{0:.2f}".format(round(percentage, 2))
-                    # we probably do not need the setting to be saved too many times on so many places, but for the sake
-                    # of keeping the structure similar, we do it here the same way,
-                    # save config to json and keep its path in the CSV
-                    conf_save_path = os.path.join(conf.train_removed_dir, identifier + ".json")
-                    with open(conf_save_path, "w", encoding="UTF-8") as conf_json:
-                        json.dump(obj=rm_setting, fp=conf_json)
-                    new_file_name = file_basename.replace(
-                        "_train.arff", "_" + identifier + "_train.arff"
+                    # if percentage is a value (not list) turn it into a list with one value
+                    percentages = (
+                        conf.percentage
+                        if isinstance(conf.percentage, list)
+                        else [conf.percentage]
                     )
 
-                    file_save_path = os.path.join(conf.train_removed_dir, new_file_name)
+                # if conf.keep_original and not 0 in percentages:
+                #         percentages.append(0)
+                for percentage in percentages:
+                    # check if base output
+                    # ds_fold_pair = os.path.basename(train_file_path).split("_")[:2]
+                    # if percentage == 0:
+                    #     if not ds_fold_pair in processed_base:
+                    #         processed_base.append(ds_fold_pair)
+                    #     else:
+                    #         continue
+                    try:
+                        if percentage == 0:
+                            new_frame = dataframe
+                        else:
+                            # here the outliers are removed
+                            rev = True if percentage < 0 else False
+                            new_frame = dataframe.select_by_od_quantile(1 - (percentage / 100), rev)
+                            new_frame.pop(OD_VALUE_NAME)
+                            new_frame._arff_data["attributes"] = [
+                                x
+                                for x in new_frame._arff_data["attributes"]
+                                if x[0] != OD_VALUE_NAME
+                            ]
+                        print("   ", train_file_path, "{}%".format(percentage))
+                        file_basename = os.path.basename(train_file_path)
+                        #name_split.insert(-1, "removed-{0:.2f}".format(round(percentage, 2)))
 
-                    new_frame.arff_dump(file_save_path)
+                        rm_setting = {"RM": percentage}
+                        # unlike in other steps, here the setting is unique by the percent number, so no hex is needed
+                        identifier = "RM{0:.2f}".format(round(percentage, 2))
+                        # we probably do not need the setting to be saved too many times on so many places, but for the sake
+                        # of keeping the structure similar, we do it here the same way,
+                        # save config to json and keep its path in the CSV
+                        conf_save_path = os.path.join(conf.train_removed_dir, identifier + ".json")
+                        with open(conf_save_path, "w", encoding="UTF-8") as conf_json:
+                            json.dump(obj=rm_setting, fp=conf_json)
+                        new_file_name = file_basename.replace(
+                            "_train.arff", "_" + identifier + "_train.arff"
+                        )
 
-                    # here we keep datasets mapping. We write it at the end to a CSV
-                    datasets_output.append(
-                        [
-                            file_save_path,
-                            test_file_path,
-                            ",".join(configs_history + [conf_save_path]),
-                        ]
-                    )
-                except KeyboardInterrupt as keyb:
-                    raise keyb
-                except Exception as exc:
-                    print(
-                        "Error:",
-                        train_file_path,
-                        "{}%".format(percentage),
-                        exc,
-                        file=sys.stderr,
-                    )
+                        file_save_path = os.path.join(conf.train_removed_dir, new_file_name)
+
+                        new_frame.arff_dump(file_save_path)
+
+                        # here we keep datasets mapping. We write it at the end to a CSV
+                        datasets_output.append(
+                            [
+                                file_save_path,
+                                test_file_path,
+                                ",".join(configs_history + [conf_save_path]),
+                            ]
+                        )
+                    except KeyboardInterrupt as keyb:
+                        raise keyb
+                    except Exception as exc:
+                        print(
+                            "Error:",
+                            train_file_path,
+                            "{}%".format(percentage),
+                            exc,
+                            file=sys.stderr,
+                        )
 
     except KeyboardInterrupt:
         print("\nInterupted!", flush=True, file=sys.stderr)
