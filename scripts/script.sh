71b0870b889b478d617374d489e4bfe175a7345d
Adam Bajger <xbajger@aura.fi.muni.cz>
2020-04-19 00:38:19 +0200

    updated script

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,56 +3,58 @@ FILE_DATASET_SPLIT="datasets-split-exp.csv"
 FILE_DATASET_OD="datasets-od-exp.csv"
 FILE_DATASET_RM="datasets-rm-exp.csv"
 FILE_DATASET_FS="datasets-fs-exp.csv"
+FILE_DATASET_CLF="datasets-clf-exp.csv"
+
 NOW=""
 NOWF=""
 
 now(){
-	NOW=$(date +"%d/%m/%Y-%H:%M:%S")
+	NOW=$(date +"%Y/%m/%d-%H:%M:%S")
 }
 nowf(){
-  NOWF=$(date +"%d%m%Y-%H%M%S")
+  NOWF=$(date +"%Y%m%d-%H%M%S")
 }
 
 now
 nowf
 TIMESTAMP_SPLIT="$NOWF"
 printf "%s - Splitting data...\n" "$NOW"
-pv056-split-data -c configs/split/default.json -do "$FILE_DATASET_SPLIT" > "logs/log-split-$TIMESTAMP_SPLIT.log"
+#pv056-split-data -c configs/split/default.json -do "$FILE_DATASET_SPLIT" > "logs/log-split-$TIMESTAMP_SPLIT.log"
 now
 printf "%s - SPLIT done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_OD="$NOWF"
 printf "%s - Applying OD methods...\n" "$NOW"
-pv056-apply-od-methods -c configs/od/default.json -di "$FILE_DATASET_SPLIT" -do "$FILE_DATASET_OD" > "logs/log-od-$TIMESTAMP_OD.log"
+#pv056-apply-od-methods -c configs/od/default.json -di "$FILE_DATASET_SPLIT" -do "$FILE_DATASET_OD" > "logs/log-od-$TIMESTAMP_OD.log"
 now
 printf "%s - OD done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_RM="$NOWF"
 printf "%s - Removing outliers...\n" "$NOW"
-pv056-remove-outliers -c configs/rm/default.json -di "$FILE_DATASET_OD"  -do "$FILE_DATASET_RM" > "logs/log-rm-$TIMESTAMP_RM.log"
+#pv056-remove-outliers -c configs/rm/default.json -di "$FILE_DATASET_OD"  -do "$FILE_DATASET_RM" > "logs/log-rm-$TIMESTAMP_RM.log"
 now
 printf "%s - RM done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_FS="$NOWF"
-printf "%s - Removing outliers...\n" "$NOW"
-pv056-evaluate-features -c configs/rm/default.json -di "$FILE_DATASET_RM"  -do "$FILE_DATASET_FS" > "logs/log-rm-$TIMESTAMP_FS.log"
+printf "%s - Evaluating features...\n" "$NOW"
+#pv056-evaluate-features -c configs/rm/default.json -di "$FILE_DATASET_RM"  -do "$FILE_DATASET_FS" > "logs/log-rm-$TIMESTAMP_FS.log"
 now
-printf "%s - RM done.\n\n" "$NOW"
+printf "%s - FS done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_CLF="$NOWF"
 printf "%s - Running classification...\n" "$NOW"
-pv056-run-clf -c configs/clf/default.json -di "$FILE_DATASET_FS" > "logs/log-clf-$TIMESTAMP_CLF.log"
+pv056-run-clf -c configs/clf/default.json -di "$FILE_DATASET_FS" -do "$FILE_DATASET_CLF" > "logs/log-clf-$TIMESTAMP_CLF.log"
 now
 printf "%s - CLF done.\n\n" "$NOW"
 
 nowf
 TIMESTAMP_ACC="$NOWF"
 printf "%s - Counting accuracy...\n" "$NOW"
-pv056-statistics -c configs/stats/default.json > "logs/log-acc-$TIMESTAMP_ACC.csv"
+#pv056-statistics -c configs/stats/default.json > "logs/log-acc-$TIMESTAMP_ACC.csv"
 now
 printf "%s - ACC done.\n\n" "$NOW"
 
