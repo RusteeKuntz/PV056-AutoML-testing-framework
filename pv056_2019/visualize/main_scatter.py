b693547d73a4be28ce50e722a7659cf6af6c4b49
AdamBajger <adambaj@seznam.cz>
2020-06-26 17:25:56 +0200

    debugging

--- pv056_2019/visualize/main_scatter.py
+++ pv056_2019/visualize/main_scatter.py
@@ -71,7 +71,11 @@ def print_nice_scatterplot(data: pd.DataFrame,
         new_col_related = col_related
 
     # now, after the related columns are joined into one, we can extract parameters
-    data[new_col_related] = data[new_col_related].map(lambda x: extract_parameter_value_as_int(x, extract_col_related))
+    if extract_col_related is not None:
+        data[new_col_related] = data[new_col_related].map(lambda x: extract_parameter_value_as_int(x, extract_col_related))
+    if extract_col_grouped_by is not None:
+        data[col_grouped_by] = data[col_grouped_by].map(
+            lambda x: extract_parameter_value_as_int(x, extract_col_grouped_by))
 
     # Scatterplot create figure
     # _fig = plt.figure( figsize=(8*scale,40*scale))
